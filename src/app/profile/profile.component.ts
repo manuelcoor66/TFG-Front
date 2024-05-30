2123cee3d2e4fa9ded0336d68768185ab933e53e
Manuel Contreras Orge <manuelcoor66@gmail.com>
2024-05-30 14:28:45 +0200

    TFG-13 Change buttons and add storage manager

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ChangePasswordModalComponent } from './change-password-modal/change-password-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss',
+  styleUrls: ['./profile.component.scss'],
+  imports: [
+    MatButton
+  ]
 })
 export class ProfileComponent {
   private dialog = inject(MatDialog);
